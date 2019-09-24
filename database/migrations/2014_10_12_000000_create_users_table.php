8276fdf2133f0ae1da71e67811503a3ef805c6db
tangtang <tang_mail@foxmail.com>
2019-09-24 16:26:55 +0800

    模型

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id');//自增长的id
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken(); //记住我  相关保存
+            $table->timestamps(); //创建时间 更新时间
         });
     }
 
